6fc034a9079175a0524182b1e4931ed19df0b992
IshviShah <73232829+IshviShah@users.noreply.github.com>
2020-11-26 11:06:18 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -6,14 +6,14 @@ class Dustbin {
       this.body = Bodies.rectangle(x,y,width,height,option);
       this.width = width;
       this.height = height;
+      this.image = loadImage("dustbingreen.png");
   
       World.add (world,this.body);
     }
       display(){
           var pos = this.body.position;
-          rectMode(CENTER);
-          fill("white");
-          rect (pos.x,pos.y,this.width,this.height);
+          imageMode(CENTER);
+          image(this.image,500,630,100,100);
       }
   
   }
